67f52496f5fd03fbc4fae5ab1d918cffa42f1a60
tiny <1533689946@qq.com>
2021-03-22 21:53:58 +0800

    21：53

--- src/main.js
+++ src/main.js
@@ -2,10 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router.js'
 
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
-
+import 'lib-flexible'
+// 导入全局样式表
+import './assets/Global.css'
+// 按需引入element
+import './plugins/element.js'
 
 
 Vue.config.productionTip = false
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,21 @@
+const px2rem = require('postcss-px2rem')
 module.exports = {
     devServer:{
         open:true
-    }
+    },
+    // 原生webpack配置
+    /* configureWebpack:{
+    } */
+    /* css: {//添加postcss配置
+        loaderOptions: {
+            postcss: {
+                plugins:[
+                    px2rem({
+                        remUnit: 75   //设计稿等分后的rem值  375/10
+                    })
+                ]
+            }
+        }
+    }, */
+    
 }
